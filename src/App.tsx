0d3c4956f11b4f564bbd7c4cb280dc7efa49ad35
mohamedabdo111 <moabdo346@gmail.com>
2025-12-09 20:48:15 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Button } from "./components/ui/button";
 import SplashCursor from "./components/SplashCursor";
 import HeroSection from "./components/features/home/heroSection";
