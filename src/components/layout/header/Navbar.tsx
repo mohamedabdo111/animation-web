8b3df091286e20d6f6cf801b0f25e5ffb80767fb
zidan <moabdo346@gmail.com>
2025-12-14 13:37:28 +0200

    fix build

--- src/components/layout/header/Navbar.tsx
+++ src/components/layout/header/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { ChevronDown, Globe } from "lucide-react";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
